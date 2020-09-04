1237f46948c3a01111d0f0bcd0ff92a910b9943c
saiyam1903 <54848181+saiyam1903@users.noreply.github.com>
2020-09-04 12:17:12 +0530

    Update question2.java

--- Assignment_1/question2.java
+++ Assignment_1/question2.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class q2 {
+public class question2 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.print("Enter Paragraph: ");
